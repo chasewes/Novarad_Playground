34a2774ad9e6fc9ff9c2c39ec1e607a268001a9d
chasewes <chasemwesthoff@gmail.com>
2022-07-08 14:21:12 -0600

    finnaly working

--- level_trace.py
+++ level_trace.py
@@ -45,125 +45,106 @@ def get_mean_val(x,y,image):
     neighbors = [image[x[0],x[1]] for x in neighbors]
     return np.mean(neighbors)
 
-def level_trace(x,y,image, threshold):
-    #check if this is a uniform area
-    #if it is, return the starting pixel
-    neighbors = get_neighbors(x,y,image)
-    neighbors = [image[x[0],x[1]] for x in neighbors]
-    if np.max(neighbors) - np.min(neighbors) < 10:
-        return [(x,y)]
-
-    #get the gradiets of 
-
-    # x, y are the starting pixels
-
-    start = (x,y)
-
-    image = np.array(image, dtype=int)
-
-    visited = set()
-
-    # #apply the threshold to the image
-    image[image < threshold] = 0
-    image[image >= threshold] = 255
-
-    start_x = x
-    start_y = y
-
-    curr_x = x
-    curr_y = y
-    
-    start_mean = get_mean_val(x,y,image)
-
-    i = 0
-
-    while True: 
-        neighbors = get_neighbors(curr_x,curr_y,image)
-        neighbors = [neighbor for neighbor in neighbors if neighbor not in visited]
-        if len(neighbors) == 0:
-            break
-        neighbor_means = [get_mean_val(neighbor[0],neighbor[1],image) for neighbor in neighbors]
-        neighbor_means = np.array(neighbor_means)
-        diffs = neighbor_means - start_mean
-        diffs = np.abs(diffs)
-        min_index = np.argmin(diffs)
-        visited.add((curr_x,curr_y))
-
-        curr_x = neighbors[min_index][0]
-        curr_y = neighbors[min_index][1]
-
-        if i == 10:
-            visited.remove((start_x,start_y))
-
-        if curr_x == start_x and curr_y == start_y:
-            break
-        i += 1
-
-    return visited
-
 def calculate_gradient(x,y,image):
     x_grad = (image[x+1,y] - image[x-1,y])/2
     y_grad = (image[x,y+1] - image[x,y-1])/2
     return np.array([x_grad, y_grad])
 
-def level_trace_2(x,y,image, threshold):
+def get_neighbors_array(x,y,image):
+    neighbors = []
+    xmin = 0
+    xmax = len(image)
+    ymin = 0
+    ymax = len(image[0])
+    neighbors = np.full((3,3),-1)
+
+    if x > 0 and y > 0:
+        neighbors[0,0] = image[x-1,y-1]
+    if x > 0 and y < ymax-1:
+        neighbors[0,2] = image[x-1,y+1]
+    if x < xmax-1 and y > 0:
+        neighbors[2,0] = image[x+1,y-1]
+    if x < xmax-1 and y < ymax-1:
+        neighbors[2,2] = image[x+1,y+1]
+    if x > 0:
+        neighbors[0,1] = image[x-1,y]
+    if x < xmax-1:
+        neighbors[2,1] = image[x+1,y]
+    if y > 0:
+        neighbors[1,0] = image[x,y-1]
+    if y < ymax-1:
+        neighbors[1,2] = image[x,y+1]
+    return neighbors
+
+def level_trace(x,y,img, threshold):
     #check if this is a uniform area
     #if it is, return the starting pixel
+    #threshold the image
+    image = img.copy()
+    image[image < threshold] = 0
+    image[image >= threshold] = 255
+
+
     neighbors = get_neighbors(x,y,image)
+    print((x,y))
+    print(neighbors)
     neighbors = [image[x[0],x[1]] for x in neighbors]
-    if np.max(neighbors) - np.min(neighbors) < 10:
+    print(neighbors)
+    if np.max(neighbors) == 0 or np.min(neighbors) == 255:
         return [(x,y)]
-    
+
     start = (x,y)
 
     image = np.array(image, dtype=int)
 
     visited = set()
+    #from the starting pixel, perform a radial sweep (moore neighborhood tracing), and add the pixels to the visited set
 
-    # #apply the threshold to the image
-    image[image < threshold] = 0
-    image[image >= threshold] = 255
+    seen_blank = False
+    curr = start
+    prev = 0
+    while curr not in visited:
+        #move clockwise around the neighbors 
+        #2 3 4
+        #1 x 5
+        #0 7 6
+        #starting at 1, look for the first pixel that is not visited that is above the threshold
+        #if you find one, add it to the visited set and continue the radial sweep
+        i = prev
+        print("outer")
+        while True:
+            i = (i+1)%8
+            if i == 0: 
+                neighbor = (curr[0]-1,curr[1]-1)
+            elif i == 1:
+                neighbor = (curr[0]-1,curr[1])
+            elif i == 2:
+                neighbor = (curr[0]-1,curr[1]+1)
+            elif i == 3:
+                neighbor = (curr[0],curr[1]+1)
+            elif i == 4:
+                neighbor = (curr[0]+1,curr[1]+1)
+            elif i == 5:
+                neighbor = (curr[0]+1,curr[1])
+            elif i == 6:
+                neighbor = (curr[0]+1,curr[1]-1)
+            else:
+                neighbor = (curr[0],curr[1]-1)
 
-
-    #make sure to check if the current pixel has a value of 255
-    #if it doesn't, change curr_x and curr_y to be a neighbor of the current pixel
-    if image[x,y] == 0:
-        neighbors = get_neighbors(x,y,image)
-        neighbors_vals = [image[x[0],x[1]] for x in neighbors]
-        min_index = np.argmin(neighbors_vals)
-        x = neighbors[min_index][0]
-        y = neighbors[min_index][1]
-
-    to_visit = []
-    while True: 
-        neighbors = get_neighbors(x,y,image)
-        neighbors = [neighbor for neighbor in neighbors if neighbor not in visited and image[neighbor] == 255]
-        if len(neighbors) == 0:
-            break
-        neighbor_vals = [image[x[0],x[1]] for x in neighbors]
-
-        neighbor_vals = np.array(neighbor_vals)
-        diffs = neighbor_vals - image[x,y]
-        diffs = np.abs(diffs)
-        min_index = np.argmin(diffs)
-        visited.add((x,y))
-
-        to_visit.append((neighbors[min_index][0],neighbors[min_index][1]))
-
-        next_thing = to_visit.pop(0)
-
-        x = next_thing[0]
-        y = next_thing[1]
-
-
-
-
+            print(f"{i}: neighbor: {neighbor}, val: {image[neighbor]}, curr: {curr}, curr_val: {image[curr]}, start: {x},{y}")
+            if image[neighbor] < threshold:
+                seen_blank = True
+            if image[neighbor] >= threshold and seen_blank:
+                visited.add(curr)
+                prev = i 
+                curr = neighbor
+                seen_blank = False
+                
+                break
+        # pdb.set_trace()
+    print(len(visited))
     return visited
 
-
-
-# function to display the coordinates of
-# of the points clicked on the image
 def click_event(event, x, y, flags, params):
 
     if event == cv2.EVENT_RBUTTONDOWN or event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_MOUSEMOVE:
@@ -172,14 +153,13 @@ def click_event(event, x, y, flags, params):
         img = cv2.GaussianBlur(cv2.imread('double-scan.jpg',0), (5,5), 0)
 		# displaying the coordinates
 		# on the Shell
-        print(x, ' ', y)
+        print(y, ' ', x)
 
-        thresh = get_initial_threshold(y,x,img)
+        # thresh = get_initial_threshold(y,x,img)
+        thresh = img[y,x]
 
         # img[img < thresh] = 0
         # img[img >= thresh] = 255
-
-        # print(img[x,y])
 
         visited = level_trace(y,x,img,thresh)
 
@@ -189,8 +169,6 @@ def click_event(event, x, y, flags, params):
             img[i] = 255
 
         cv2.imshow('image', img)
-
-
 
 
 
